d58cfe7b39432c23d7fb839d3a2c51e1ab44b41c
ju wang <jwang@vsu.edu>
2025-11-08 21:28:45 -0500

    up

--- run_clientlog.sh
+++ run_clientlog.sh
@@ -1,3 +1,3 @@
 python3 -m motion.tool --log-level info --base http://dex:9050 quick --control xbox --file franka_simplestack_flat.usd --runner counter --effector /World/Franka/panda_hand --device cuda --no-tick --gripper /World/Franka/panda_hand/panda_finger_joint1 --gripper /World/Franka/panda_hand/panda_finger_joint2 2>&1 | tee tt2.txt
-cat tt2.txt |grep "Step=" |awk -F= '{print $2}'
+cat tt2.txt |grep "Step=" |awk -F= '{print $2}' > ttt_client.txt
 
--- run_runnerlog.sh
+++ run_runnerlog.sh
@@ -1,2 +1,2 @@
 docker logs -f motion-runner 2>&1 | tee ttt.txt
-cat ttt.txt |grep "send=" |awk -F= '{print $2}'
+cat ttt.txt |grep "recv=" |awk -F= '{print $2}' > ttt_runner.txt
